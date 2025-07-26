41546686da2d94f2ced2fa6450460957dd3ff3ae
ROKR7381 <roshan.kr.hit@gmail.com>
2025-07-26 22:55:51 +0530

    folder structure updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Document_Portal",
+    author="Roshan Kumar",
+    version="0.1",
+    packages=find_packages(),
+    include_package_data=True,
+)
